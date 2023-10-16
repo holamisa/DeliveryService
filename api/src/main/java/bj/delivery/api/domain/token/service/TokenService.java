259767de02793fb95894b7d2751b8e96fe441c1e
holamisa <bongjaejeong@naver.com>
2023-10-17 07:17:53 +0900

    1. 생성자 수정

--- api/src/main/java/bj/delivery/api/domain/token/service/TokenService.java
+++ api/src/main/java/bj/delivery/api/domain/token/service/TokenService.java
@@ -2,7 +2,7 @@ package bj.delivery.api.domain.token.service;
 
 import bj.delivery.api.common.error.ErrorCode;
 import bj.delivery.api.common.exception.ApiException;
-import bj.delivery.api.domain.token.ifs.TokenHelperIfs;
+import bj.delivery.api.domain.token.helper.JwtTokenHelper;
 import bj.delivery.api.domain.token.model.TokenDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,24 +14,24 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class TokenService {
 
-    private final TokenHelperIfs tokenHelperIfs;
+    private final JwtTokenHelper tokenHelper;
 
     public TokenDTO issueAccessToken(Long userId){
         var data = new HashMap<String, Object>();
         data.put("userId", userId);
 
-        return tokenHelperIfs.issueAccessToken(data);
+        return tokenHelper.issueAccessToken(data);
     }
 
     public TokenDTO issueRefreshToken(Long userId){
         var data = new HashMap<String, Object>();
         data.put("userId", userId);
 
-        return tokenHelperIfs.issueRefreshToken(data);
+        return tokenHelper.issueRefreshToken(data);
     }
 
     public Long validationToken(String token){
-        var map = tokenHelperIfs.validationTokenWithThrow(token);
+        var map = tokenHelper.validationTokenWithThrow(token);
 
         var userId = map.get("userId");
 
