085a6af1453b57409d2482497f274adf469110ab
holamisa <bongjaejeong@naver.com>
2023-10-17 05:31:25 +0900

    불필요한 파일 삭제

--- db/src/main/java/bj/delivery/db/account/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package bj.delivery.db.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
-}
